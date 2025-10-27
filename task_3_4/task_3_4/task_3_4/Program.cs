c4d1a09d15a228f15d757206c468d45029fc6a20
kidesik <elizafox2005@gmail.com>
2025-10-27 18:22:43 +0300

    Add files via upload

---
+++ task_3_4/task_3_4/task_3_4/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите количество школьников");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество яблок");
+int k  = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вот сколько яблок достанется каждому школьнику: " + (k / N) + "\n" + "Вот сколько яблок останется в корзинке: " + (k % N));
