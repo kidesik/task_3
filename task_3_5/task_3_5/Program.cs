17c87589bbdb48f78ed96af6307660bd08b89612
kidesik <elizafox2005@gmail.com>
2025-10-27 18:21:42 +0300

    Add files via upload

---
+++ task_3_5/task_3_5/Program.cs
@@ -0,0 +1,10 @@
+﻿//размеры прямоугольника
+int length = 130;
+int width = 543;
+//Размеры квадратов
+int square = 130;
+//количество квадратов
+int num_of_squares_in_width = width/ square;
+int num_of_squares_in_length = length / square;
+int num_of_squares = num_of_squares_in_length * num_of_squares_in_width;
+Console.WriteLine("Количество квадратов которое можно отрезать от прямоугольника 543*130: " + num_of_squares);
