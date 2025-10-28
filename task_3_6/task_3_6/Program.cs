f07fc7ae997cd689e21197e9dcdfad382144fa12
kidesik <elizafox2005@gmail.com>
2025-10-28 18:07:13 +0300

    Add files via upload

---
+++ task_3_6/task_3_6/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите номер вашего сиденья");
+int number = Convert.ToInt32(Console.ReadLine());
+int compartment_num = (number - 1) / 4 + 1;
+Console.WriteLine("Номер вашего купе: " + compartment_num);
