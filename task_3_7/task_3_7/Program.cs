636541a3c3b2e84818d2fb96d9dcb6fc7ed26cc7
kidesik <elizafox2005@gmail.com>
2025-10-29 18:08:29 +0300

    Add files via upload

---
+++ task_3_7/task_3_7/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите номер квартиры");
+int apartment = Convert.ToInt32(Console.ReadLine());
+if (apartment <= 15)
+{
+    int floor = (apartment - 1) / (15 / 5) + 1;
+    Console.WriteLine("Квартира " + apartment + " находится на " + floor);
+}
+else
+{
+    Console.WriteLine("Эта квартира находится в другом подъезде");
+}
