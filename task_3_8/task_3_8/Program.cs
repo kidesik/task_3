1fbb361e8bd1f9e0e2447a492ccc2cbab80645a8
kidesik <elizafox2005@gmail.com>
2025-10-30 12:35:45 +0300

    Add files via upload

---
+++ task_3_8/task_3_8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите номер места");
+int serial_number = Convert.ToInt32(Console.ReadLine());
+int seat_number = serial_number - 01643 + 1;
+int row_number = (seat_number / 15);
+if (row_number == 0)
+{
+    row_number += 1;
+}
+Console.WriteLine("Ваше место " + seat_number + " находится в " + row_number + " ряду");
