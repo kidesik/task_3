1913af74cd81c995e414bc22780e1c6f1780e470
kidesik <elizafox2005@gmail.com>
2025-12-04 16:18:35 +0300

    Add files via upload

---
+++ task_3_12/task_3_12/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите номер квартиры:");
+int number_kv = Convert.ToInt32(Console.ReadLine());
+int floors = ((number_kv - 1)/ (4 + 1))+ 1;
+int pos = ((number_kv - 1)% 4) + 1;
+Console.WriteLine("Этаж:" + floors + "\n Позиция на этаже:" + pos);
---
+++ task_3_13/task_3_13/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите номер значения: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int s = ((n - 1) / 5) + 1;
+int p = ((n - 1) % 5) + 1;
+Console.WriteLine("Номер строки: " + s + "\nНомер столбца: " + p);
