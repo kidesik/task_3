9e2a478a549bdc6adb0bd7cd3fa2901afbcca6cf
kidesik <elizafox2005@gmail.com>
2025-12-06 15:04:28 +0300

    Add files via upload

---
+++ task_3_20/task_3_20/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = n % 100 / 10;
+int  b = n % 100 % 10;
+int sum = a + b;
+int g = a * b;
+Console.WriteLine("Количество десятков: " + a + "\nКоличество едениц: " + b + "\nСумма едениц и десятков: " + sum + "\nПроизведение едениц и десятков: " + g);
+int c = n / 100;
+Console.WriteLine("Количество сотен: " + c + "\nСумма всех цифр: " + (a + b + c) + "\nПроизведение всех цифр: " + (a * b * c));
---
+++ task_3_22/task_3_22/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = n / 100;
+int b = n % 100 / 10;
+int  c = n % 100 % 10;
+Console.WriteLine(b + "" + c + "" + a);
