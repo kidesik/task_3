86d0c0014e11fdbf92e1964d5efd57d9deb386a2
kidesik <elizafox2005@gmail.com>
2025-12-28 14:55:42 +0300

    Add files via upload

---
+++ task_3_50/task_3_50/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите часы и минуты");
+int h = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+double pos_h = (h % 12) * 30 + m * 0.5;
+int pos_m = m * 6;
+double v_real = 5.5;
+double diff_a = (pos_h - pos_m) % 360;
+if (diff_a == 0){
+    diff_a = 360;
+    double t_a = diff_a / v_real;
+    int resultA = (int)Math.Ceiling(t_a);
+    Console.WriteLine($"До совпадения: {resultA} мин.");
+
+}
+else{
+    double t_a = diff_a / v_real;
+    int resultA = (int)Math.Ceiling(t_a);
+    Console.WriteLine($"До совпадения: {resultA} мин.");
+}
+double diff_b1 = (pos_h - pos_m - 90) % 360;
+double diff_b2 = (pos_h - pos_m - 270) % 360;
+
+if (diff_b1 <= 0) diff_b1 += 360;
+if (diff_b2 <= 0) diff_b2 += 360;
+
+double t_b1 = diff_b1 / v_real;
+double t_b2 = diff_b2 / v_real;
+
+double t_b = Math.Min(t_b1, t_b2);
+int resultB = (int)Math.Ceiling(t_b);
+
+Console.WriteLine($"До перпендикулярности: {resultB} мин.");
---
+++ task_3_51/task_3_51/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите два целых числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine((a % b) * (b % a) + 1);
